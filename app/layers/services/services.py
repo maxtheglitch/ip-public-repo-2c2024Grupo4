e2d12d796f40a90325cf04c4f0d0df9f7dbca969
unknown <maximiliano.abel.duarte@gmail.com>
2024-11-13 21:42:44 -0300

    Se agregó boostrap (Carpetas css y js en la carpeta templates) y se agregaron las lineas de código necesarias para visualizar las cards en services.py(Lineas 11 a 20, service.py)

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -1,16 +1,22 @@
 # capa de servicio/lógica de negocio
 
+from asyncio import Transport
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
+from ..transport import transport
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
+    json_collection = transport.getAllImages()
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
 
+    for objeto in json_collection:
+        Dato=translator.fromRequestIntoCard(objeto)
+        images.append(Dato)
+    
     return images
 
 # añadir favoritos (usado desde el template 'home.html')
